99380afa830422c9776c624e6fdfc369479c7c85
Alex Buchanan <buchanae@gmail.com>
2013-06-23 14:44:00 -0700

    initial

---
+++ recycle.py
@@ -0,0 +1,26 @@
+import itertools
+
+
+class Recycleable(object):
+    def __init__(self, fn, *args, **kwargs):
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    def __iter__(self):
+        return self.fn(*self.args, **self.kwargs)
+
+def recycleable(fn):
+    def wrapper(*args, **kwargs):
+        return Recycleable(fn, *args, **kwargs)
+    return wrapper
+
+@recycleable
+def map(fn, *iterables):
+    return itertools.imap(fn, *iterables)
+
+@recycleable
+def chain(*iterables):
+    return itertools.chain.from_iterable(iterables)
+
+chain.from_iterable = lambda iterable: chain(*iterable)
---
+++ tests.py
@@ -0,0 +1,69 @@
+import nose
+from nose.tools import eq_
+
+import recycle
+
+
+def gen(length):
+    for x in xrange(length):
+        yield x
+
+
+@recycle.recycleable
+def decorated_gen(length):
+    return gen(length)
+
+
+def test_Recycleable():
+
+    # This just demonstrates that normally the generator
+    # isn't reusable.
+    g = gen(5)
+    eq_([x for x in g], [0, 1, 2, 3, 4])
+    eq_([x for x in g], [])
+
+    # Wrapping it in a Recyclable means that it can be iterated over multiple times.
+    r = recycle.Recycleable(gen, 5)
+    eq_([x for x in r], [0, 1, 2, 3, 4])
+    eq_([x for x in r], [0, 1, 2, 3, 4])
+
+
+def test_recycle():
+    # Test the decorator
+
+    r = decorated_gen(5)
+    eq_([x for x in r], [0, 1, 2, 3, 4])
+    eq_([x for x in r], [0, 1, 2, 3, 4])
+
+
+def test_map():
+
+    map_func = lambda x: x * 2
+
+    r = decorated_gen(5)
+    r = recycle.map(map_func, r)
+
+    eq_([x for x in r], [0, 2, 4, 6, 8])
+    eq_([x for x in r], [0, 2, 4, 6, 8])
+    
+
+def test_chain():
+    a = decorated_gen(5)
+    b = decorated_gen(5)
+    r = recycle.chain(a, b)
+
+    eq_([x for x in r], [0, 1, 2, 3, 4, 0, 1, 2, 3, 4])
+
+
+def test_chain_and_map():
+
+    map_func = lambda x: x * 2
+
+    a = decorated_gen(5)
+    b = decorated_gen(5)
+    c = recycle.chain(a, b)
+    r = recycle.map(map_func, c)
+    eq_([x for x in r], [0, 2, 4, 6, 8, 0, 2, 4, 6, 8])
+
+if __name__ == '__main__':
+    nose.run()
