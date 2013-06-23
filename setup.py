4c2d37acd91ce11d3f6b5cd9a3fd1e5afe85b260
Alex Buchanan <buchanae@gmail.com>
2013-06-23 14:45:05 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+import recycle
+
+
+setup(
+    name='recycle',
+    description='TODO',
+    long_description=open('README.md').read(),
+    version=recycle.__version__,
+    author='Alex Buchanan',
+    author_email='buchanae@gmail.com',
+    license='BSD',
+    py_modules=['recycle']
+)
